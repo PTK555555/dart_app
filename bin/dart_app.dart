688de066ca12042a69118e7e01abf4811b1b62a0
PTK555555 <6631501077@lamduan.mfu.ac.th>
2025-08-27 17:40:09 +0700

    Plan feature

--- bin/dart_app.dart
+++ bin/dart_app.dart
@@ -3,5 +3,18 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 
 void main() async {
-  
+  // Login Function
+  // menu feature
+      
+      // 1. Show all
+
+        // 2. Today's expense
+
+        // 3. Search expense
+
+        // 4. Add new expense
+
+        // 5. Delete an expense
+
+        // 6. Exit
 } 
